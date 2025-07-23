9ca833b890b6c2e69fda6fe1357fb24f70a41308
hasin.pramodya <pramodyahasin@gmail.com>
2025-07-23 23:00:30 +0530

    Connect the backend project with mongo db cluster

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
 
+dotenv.config();
+
 app.listen(port,()=>{
     console.log(`Server is started on :${port}`);
+    mongoose.connect(process.env.MONGODB_URL).then(
+        ()=>{
+            console.log("Connected to MongoDB Database successfully");
+        }
+    ).catch(
+        (err)=>{
+            console.error("Connection Failed:", err);
+        }
+    )
+
 })
 
 app.use(bodyParser.json());
