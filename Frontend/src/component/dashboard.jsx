69eb20b87bda401509e3bbee85133568502bd03d
hasin.pramodya <pramodyahasin@gmail.com>
2025-08-01 03:08:29 +0530

    add dashboard component

---
+++ Frontend/src/component/dashboard.jsx
@@ -0,0 +1,12 @@
+export default function DashboardPage() {
+    return(
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+                <h1 className="text-3xl font-bold  justify-center text-center  mb-10">
+                    Admin Dashboard
+                </h1>
+                <img src="src/assets/images/dashboard.jpg" alt="Dashboard Image" className="mx-auto h-[500px]" />
+            </div>
+        </>
+    )
+}
